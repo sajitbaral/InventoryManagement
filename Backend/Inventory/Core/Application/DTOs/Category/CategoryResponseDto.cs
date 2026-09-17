b044a44b8ddeb01fd32b69c2488a2ba7235d0e7a
Sajit Baral <sajit.baral.cloud@gmail.com>
2026-09-17 17:31:12 +0545

    Created category dto.

---
+++ Backend/Inventory/Core/Application/DTOs/Category/CategoryResponseDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Application.DTOs.Category
+{
+    public class CategoryResponseDto
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
---
+++ Backend/Inventory/Core/Application/DTOs/Category/CreateCategoryDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Inventory.Application.DTOs.Category
+{
+    public class CreateCategoryDto
+    {
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        public string? Description { get; set; }
+    }
+}
