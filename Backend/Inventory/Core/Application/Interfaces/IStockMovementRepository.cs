a568ec7cf83a1a2a4afe315f0794cd26ac62b4e3
Sajit Baral <sajit.baral.cloud@gmail.com>
2026-09-23 18:28:22 +0545

    Created stockmovement repository layer.

---
+++ Backend/Inventory/Core/Application/Interfaces/IStockMovementRepository.cs
@@ -0,0 +1,16 @@
+﻿using Inventory.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Application.Interfaces
+{
+    public interface IStockMovementRepository
+    {
+        Task<List<StockMovement>> GetStockMovementsAsync(CancellationToken cancellationToken);
+        Task<StockMovement?> GetByIdAsync(int stockMovementId, CancellationToken cancellationToken);
+        Task <List<StockMovement>> GetByProductIdAsync(int productId, CancellationToken cancellationToken);
+        Task AddAsync(StockMovement stockMovement);
+
+    }
+}
---
+++ Backend/Inventory/Infrastructure/Repositories/StockMovementRepository.cs
@@ -0,0 +1,50 @@
+﻿using Inventory.Application.Interfaces;
+using Inventory.Domain.Entities;
+using Inventory.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Infrastructure.Repositories
+{
+    public class StockMovementRepository : IStockMovementRepository
+    {
+        private readonly InventoryDbContext _context;
+        public StockMovementRepository(InventoryDbContext context)
+        {
+            _context= context;
+        }
+
+        public async Task<List<StockMovement>> GetStockMovementsAsync(CancellationToken cancellationToken)
+        {
+            return await _context.StockMovements
+                .ToListAsync(cancellationToken);
+
+        }
+
+        public async Task<StockMovement?> GetByIdAsync(int stockMovementId, CancellationToken cancellationToken)
+        {
+            var stockMovement= await _context.StockMovements
+                .FirstOrDefaultAsync(sm=> sm.StockMovementId == stockMovementId, cancellationToken);
+
+            return stockMovement;
+        }
+
+        public async Task<List<StockMovement>> GetByProductIdAsync(int productId, CancellationToken cancellationToken)
+        {
+            var stockMovements = await _context.StockMovements
+                .Where(sm => sm.ProductId == productId)
+                .ToListAsync(cancellationToken);
+
+            return stockMovements;
+
+        }
+
+        public async Task AddAsync(StockMovement stockMovement)
+        {
+            await _context.StockMovements.AddAsync(stockMovement);
+        }
+
+    }
+}
