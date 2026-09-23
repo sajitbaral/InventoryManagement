d67acb2363f2537b5a5a3167ab34fc8fa9606eef
Sajit Baral <sajit.baral.cloud@gmail.com>
2026-09-23 17:17:19 +0545

    Created stockmovement dto

---
+++ Backend/Inventory/Core/Application/DTOs/StockMovement/StockMovementResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using Inventory.Domain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace Inventory.Application.DTOs.StockMovement
+{
+    public class StockMovementResponseDto
+    {
+        public int StockMovementId { get; set; }
+        public int ProductId { get; set; }
+        public MovementType MovementType { get; set; }
+        public int Quantity { get; set; }
+        public DateTime MovementDate { get; set; }
+        public int? ReferenceId { get; set; }
+        public AdjustmentType? AdjustmentType { get; set; }
+    }
+}
