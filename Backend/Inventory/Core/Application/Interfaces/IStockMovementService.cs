bfe8c380be98f0cfa9afafd6f06bc36976557f0d
Sajit Baral <sajit.baral.cloud@gmail.com>
2026-09-23 22:10:10 +0545

    Created StockMovement service layer.

---
+++ Backend/Inventory/Core/Application/Interfaces/IStockMovementService.cs
@@ -0,0 +1,14 @@
+﻿using Inventory.Application.DTOs.StockMovement;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Application.Interfaces
+{
+    public interface IStockMovementService
+    {
+        Task<List<StockMovementResponseDto>> GetStockMovementsAsync(CancellationToken cancellationToken);
+        Task<StockMovementResponseDto?> GetByIdAsync(int stockMovementId,  CancellationToken cancellationToken);
+        Task<List<StockMovementResponseDto>> GetByProductIdAsync(int productId, CancellationToken cancellationToken);
+    }
+}
---
+++ Backend/Inventory/Core/Application/Services/StockMovementService.cs
@@ -0,0 +1,73 @@
+﻿using Inventory.Application.DTOs.StockMovement;
+using Inventory.Application.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Inventory.Application.Services
+{
+    public class StockMovementService : IStockMovementService
+    {
+        private readonly IStockMovementRepository _stockMovementRepository;
+        public StockMovementService(IStockMovementRepository stockMovementRepository)
+        {
+            _stockMovementRepository = stockMovementRepository;
+            
+        }
+
+        public async Task<List<StockMovementResponseDto>> GetStockMovementsAsync(CancellationToken cancellationToken)
+        {
+            var stockMovements= await _stockMovementRepository.GetStockMovementsAsync(cancellationToken);
+
+            return stockMovements.Select(sm => new StockMovementResponseDto
+            {
+                StockMovementId = sm.StockMovementId,
+                ProductId = sm.ProductId,
+                MovementType = sm.MovementType,
+                Quantity = sm.Quantity,
+                MovementDate = sm.MovementDate,
+                ReferenceId = sm.ReferenceId,
+                AdjustmentType = sm.AdjustmentType
+            })
+                .ToList();
+        }
+
+        public async Task<StockMovementResponseDto?> GetByIdAsync(int stockMovementId, CancellationToken cancellationToken)
+        {
+            var stockMovement = await _stockMovementRepository.GetByIdAsync(stockMovementId, cancellationToken);
+
+            if (stockMovement == null)
+            {
+                return null;
+            }
+
+            return new StockMovementResponseDto
+            {
+                StockMovementId = stockMovement.StockMovementId,
+                ProductId = stockMovement.ProductId,
+                MovementType = stockMovement.MovementType,
+                Quantity = stockMovement.Quantity,
+                MovementDate = stockMovement.MovementDate,
+                ReferenceId = stockMovement.ReferenceId,
+                AdjustmentType = stockMovement.AdjustmentType
+            };
+        }
+
+        public async Task<List<StockMovementResponseDto>> GetByProductIdAsync(int productId,  CancellationToken cancellationToken)
+        {
+            var stockMovements = await _stockMovementRepository.GetByProductIdAsync(productId, cancellationToken);
+            return stockMovements.Select(sm => new StockMovementResponseDto
+            {
+                StockMovementId = sm.StockMovementId,
+                ProductId = sm.ProductId,
+                MovementType = sm.MovementType,
+                Quantity = sm.Quantity,
+                MovementDate = sm.MovementDate,
+                ReferenceId = sm.ReferenceId,
+                AdjustmentType = sm.AdjustmentType
+            })
+                .ToList();
+        }
+
+    }
+}
